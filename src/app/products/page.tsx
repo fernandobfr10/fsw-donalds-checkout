199ec3e661fcc82471b09f6be7e662dc88e8aeeb
Fernando Junior <fernandobfr10@gmail.com>
2025-02-18 16:25:49 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>ProductsPage</h1>
+      <Button>Click Me!</Button>
+    </div>
+  )
+}
