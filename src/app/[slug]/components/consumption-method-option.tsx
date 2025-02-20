4509ae99ee5ed8f7248ca48d9fbc4282dd77ee95
Fernando Junior <fernandobfr10@gmail.com>
2025-02-20 00:55:35 -0300

    Lesson 3 completed

---
+++ src/app/[slug]/components/consumption-method-option.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import type { ConsumptionMethod } from '@prisma/client'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+
+interface ConsumptionMethodOptionProps {
+  imageUrl: string
+  imageAlt: string
+  buttonText: string
+  option: ConsumptionMethod
+  slug: string
+}
+
+const ConsumptionMethodOption = ({
+  imageUrl,
+  imageAlt,
+  buttonText,
+  option,
+  slug,
+}: ConsumptionMethodOptionProps) => {
+  return (
+    <Card>
+      <CardContent className="flex flex-col items-center gap-8 py-8">
+        <div className="relative h-[80px] w-[80px]">
+          <Image
+            className="object-contain"
+            src={imageUrl}
+            alt={imageAlt}
+            width={78}
+            height={80}
+          />
+        </div>
+        <Button variant="secondary" className="rounded-full" asChild>
+          <Link href={`/${slug}/menu?consumptionMethod=${option}`}>
+            {buttonText}
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export { ConsumptionMethodOption }
---
+++ src/app/[slug]/menu/components/header.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import type { Restaurant } from '@prisma/client'
+import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react'
+import Image from 'next/image'
+
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+
+interface RestaurantMenuHeaderProps {
+  restaurant: Pick<Restaurant, 'coverImageUrl' | 'name'>
+}
+
+const RestaurantMenuHeader = ({ restaurant }: RestaurantMenuHeaderProps) => {
+  const router = useRouter()
+
+  const handleGoBack = () => router.back()
+
+  return (
+    <div className="relative h-[250px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 left-4 rounded-full z-50"
+        onClick={handleGoBack}
+      >
+        <ChevronLeftIcon />
+      </Button>
+
+      <Image
+        className="object-cover"
+        src={restaurant.coverImageUrl}
+        alt={restaurant.name}
+        fill
+      />
+
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute top-4 right-4 rounded-full z-50"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  )
+}
+
+export { RestaurantMenuHeader }
